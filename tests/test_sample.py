64b0dfeb81027243e0c1ee7bb0ad37dd8b61ae33
aqiu <819110812@qq.com>
2023-03-21 20:33:22 +0800

    rename file

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,2 +1,16 @@
 def test_sample():
     return 1 == 1
+<<<<<<< Updated upstream
+=======
+
+
+def test_sample2():
+    return 1 == 1
+
+
+def test_sample3():
+    return 1 == 1
+
+def test_sample4():
+    return 1 == 1
+>>>>>>> Stashed changes
